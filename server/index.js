12673fceb1e75051f7be81655b9b46d6e48564ef
tiff0000 <tiffany.yang@mail.utoronto.ca>
2021-05-24 14:08:53 -0400

    redux implemented

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-// set the starting path for all routes inside posts. Means all routes inside posts, prepend this /posts infront of their specified path e.g. get /home is => get /posts/home
-app.use('/posts', postRoutes); // localhost:5000/posts
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// set the starting path for all routes inside posts. Means all routes inside posts, prepend this /posts infront of their specified path e.g. get /home is => get /posts/home
+app.use('/posts', postRoutes); // localhost:5000/posts
 
 const CONNECTION_URL = 'mongodb+srv://tiffanydev:zN75qWYzfxPUQVG5@cluster0.rste5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
